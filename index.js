b9c79d8513091d333f5e81ddce8c646b637d43d5
Reema Dhanwani <reemadhanwani1234@gmail.com>
2023-04-14 21:19:09 +0530

    Basic room setup done

--- index.js
+++ index.js
@@ -4,21 +4,32 @@ const http = require('http');
 const server = http.createServer(app);
 const socketio = require('socket.io');
 const io = socketio(server);
-
+const connect  = require('./config/database-config');
 
 io.on('connection',(socket) => {
-    // console.log('A user connected',socket.id);
-     socket.on('msg_send',(data)=>{
-         console.log(data);
-         //io.emit('msg_rcvd',data);
-         //socket.emit('msg_rcvd',data);
-         socket.broadcast.emit('msg_rcvd',data);
+    socket.on('join_room', (data) => {
+        console.log("joining a room", data.roomid)
+        socket.join(data.roomid);
+    });
+
+     socket.on('msg_send',async (data)=>{
+         io.to(data.roomid).emit('msg_rcvd',data);
      });
+     
  });
 
+app.set('view engine','ejs');
 
+app.get('/chat/:roomid',(req,res)=>{
+    res.render('index',{
+        name: 'Reema',
+        id: req.params.roomid
+    });
+})
 app.use('/',express.static(__dirname+'/public'));
 
-server.listen(3000,()=> {
+server.listen(3000,async ()=> {
     console.log('Server Started');
+    await connect();
+    console.log('mongodb connected');
 })
