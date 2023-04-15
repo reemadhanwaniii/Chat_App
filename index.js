828c0f7738734b4091103cb6dcb090e59d4df1e6
Reema Dhanwani <reemadhanwani1234@gmail.com>
2023-04-15 13:53:12 +0530

    Complete basic chat app using sockets

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const server = http.createServer(app);
 const socketio = require('socket.io');
 const io = socketio(server);
 const connect  = require('./config/database-config');
+const Chat = require('./model/chat');
 
 io.on('connection',(socket) => {
     socket.on('join_room', (data) => {
@@ -13,17 +14,29 @@ io.on('connection',(socket) => {
     });
 
      socket.on('msg_send',async (data)=>{
+         const chat = await Chat.create({
+            roomId: data.roomid,
+            user: data.username,
+            content: data.msg
+         });
          io.to(data.roomid).emit('msg_rcvd',data);
      });
-     
+     socket.on('typing', (data) => {
+        socket.broadcast.to(data.roomid).emit('someone_typing');
+    });
  });
 
 app.set('view engine','ejs');
 
-app.get('/chat/:roomid',(req,res)=>{
+app.get('/chat/:roomid',async (req,res)=>{
+    const chats = await Chat.find({
+        roomId: req.params.roomid
+    }).select('content user');
+    console.log(chats);
     res.render('index',{
         name: 'Reema',
-        id: req.params.roomid
+        id: req.params.roomid,
+        chats: chats
     });
 })
 app.use('/',express.static(__dirname+'/public'));
